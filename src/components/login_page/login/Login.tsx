9d33ac0a2c32f84330a83893d80ac170125754ea
unknown <baochungnguyenngoc@gmail.com>
2022-04-09 18:04:30 +0700

    first commit

---
+++ src/components/login_page/login/Login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+Login.propTypes = {};
+
+function Login() {
+  return <div></div>;
+}
+
+export default Login;
---
+++ src/components/login_page/password/Password.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+Password.propTypes = {};
+
+function Password() {
+  return <div></div>;
+}
+
+export default Password;
